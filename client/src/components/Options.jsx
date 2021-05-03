cdf254e4235ea09839d1b33a1ec0a9aedc687562
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-05-03 13:41:20 +0530

    Added logic for Video Player component

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button , TextField , Grid , Typography , Container , Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
 const Options = ({children}) => {
     return(
